9a4350d89f963e1d4884bf75e222996e47d8932b
margusraudsepp <mcklapp@hotmail.com>
2014-03-29 03:35:34 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 
 define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '1y80m9');
+define('DATABASE_USERNAME', '');
+define('DATABASE_PASSWORD', '');
 define('DATABASE_DATABASE', 'blog');
 define('DEFAULT_CONTROLLER', 'posts');
 define('BASE_URL', '/blog/');
